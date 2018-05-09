56579885e401f5b98ff97967a75a59bd28389630
GrandChallengeChem <38963588+GrandChallengeChem@users.noreply.github.com>
2018-05-08 20:17:21 -0400

    Update jquery.js

--- docs/jQuery/jquery.js
+++ docs/jQuery/jquery.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
   $( "#hmsp" ).tooltip({
-    item: "span#hmsp",
+    items: "span#hmsp",
     content: "The binomial name Homo sapiens was coined by Carl Linnaeus (1758).[3] The Latin noun homō (genitive hominis) means "human being", while the participle sapiēns means "discerning, wise, sensible".",
     track: true
   });
