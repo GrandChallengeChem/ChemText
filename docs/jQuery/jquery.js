07a2390dbbe5517a35a7081f37ccae0954430604
GrandChallengeChem <38963588+GrandChallengeChem@users.noreply.github.com>
2018-05-30 22:56:55 -0400

    Add files via upload

--- docs/jQuery/jquery.js
+++ docs/jQuery/jquery.js
@@ -6,3 +6,22 @@ $(document).ready(function(){
     track: true
   });
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
