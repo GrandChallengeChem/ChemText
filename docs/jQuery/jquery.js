75de9c1c7ed3172a119b05fef15d89a2e0f1267f
GrandChallengeChem <38963588+GrandChallengeChem@users.noreply.github.com>
2018-06-01 15:26:05 -0400

    Add files via upload

--- docs/jQuery/jquery.js
+++ docs/jQuery/jquery.js
@@ -7,10 +7,17 @@ $(document).ready(function(){
   });
 });
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
 }
 
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
