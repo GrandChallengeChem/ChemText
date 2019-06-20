4534871618ad0366d227ca34883ce22c17dc7988
GrandChallengeChem <38963588+GrandChallengeChem@users.noreply.github.com>
2019-06-20 16:58:26 -0400

    Add files via upload

--- docs/jQuery/jquery.js
+++ docs/jQuery/jquery.js
@@ -11,45 +11,61 @@ $(document).ready(function(){
   });
 });
 
-/* -------- Energy 1 -------- */
+/* -------- Book Content -------- */
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function DropdownLevels() {
-  document.getElementById("dropdown-levels").classList.toggle("show");
-}
+/* Sidenav Style */
 
-function DropdownChallenges() {
-  document.getElementById("dropdown-challenges").classList.toggle("show");
-}
-
-/* Close the dropdown if the user clicks outside of it */
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("dropdown-levels");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
-
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("dropdown-challenges");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
-
-/* Sidebar */
-
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("side-content").style.width = "250px";
-  document.getElementById("main-text").style.marginLeft = "250px";
+  document.getElementById("sidenav").style.width = "250px";
 }
 
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("side-content").style.width = "0";
-  document.getElementById("main-text").style.marginLeft= "0";
+  document.getElementById("sidenav").style.width = "0";
+}
+
+/* Submenu Sidebar Style */
+
+//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+} 
+
+/* SideTool Style */
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openTool() {
+  document.getElementById("sidetool").style.width = "80px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeTool() {
+  document.getElementById("sidetool").style.width = "0";
+}
+
+/** Function displaying the tooltip when cursor is hovered over the icon **/
+$(document).ready(function(){
+  
+  $( "#prdtbl" ).tooltip({
+    items: "span#prdtbl",
+    content: "Periodic Table",
+    track: true
+  });
+});
+
+/** Function that sets the periodic table image to 'block' **/
+function modalfunc() {
+  document.getElementById('modal01').style.display='block';
 }
